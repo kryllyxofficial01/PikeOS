240e09cb921d6b29142c25c13caf2f4b046c478a
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-04-03 22:19:41 -0400

    reading from disk

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -32,47 +32,141 @@ system_id: db " FAT12 "
 start:
     jmp main
 
-; Save the register values before they're modified
 puts:
     push si
     push ax
+    push bx
 
-loop:
+.loop:
     lodsb
     or al, al
-    jz done
+    jz .done
 
     mov ah, 0x0e
     mov bh, 0
     int 0x10
 
-    jmp loop
+    jmp .loop
 
-done:
+
+.done:
+    pop bx
     pop ax
     pop si
+
     ret
 
 main:
-    ; Setup data segments
     mov ax, 0
     mov ds, ax
     mov es, ax
 
-    ; Setup stack
     mov ss, ax
     mov sp, 0x7c00
 
-    ; Print message
+    mov [drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7e00
+    call read_from_disk
+
     mov si, message
     call puts
 
+    cli
     hlt
 
-halt:
-    jmp halt
+disk_read_failed:
+    mov si, floppy_error
+    call puts
+    jmp wait_to_reboot
+
+wait_to_reboot:
+    mov ah, 0
+    int 16h
+    jmp 0ffffh:0
+
+.halt:
+    cli
+    hlt
+
+lba_to_chs:
+    push ax
+    push dx
+
+    xor dx, dx
+    div word [track_sectors]
+
+    inc dx
+    mov cx, dx
+
+    xor dx, dx
+    div word [heads]
+
+    mov dh, al
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov dl, al
+    pop ax
+
+    ret
+
+read_from_disk:
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx
+    call lba_to_chs
+    pop ax
+
+    mov ah, 02h
+    mov di, 3
+
+.retry:
+    pusha
+    stc
+    int 13h
+    jnc .done
+
+    popa
+    call reset_disk
+
+    dec di
+    test di, di
+    jnz .retry
+
+.fail:
+    jmp disk_read_failed
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+
+    ret
+
+reset_disk:
+    pusha
+    mov ah, 0
+    stc
+    int 13h
+    jc disk_read_failed
+    popa
+
+    ret
 
 message: db "test", ENDL, 0
+floppy_error: db "Failed to read from disk", ENDL, 0
 
 times 510-($-$$) db 0 ; Pad the program with zeros so it creates a 512 byte sector
-dw 0aa55h ; BIOS signature
+dw 0x0aa55 ; BIOS signature
