6298d22e331f87175eba5eef7f5873f3d889de7c
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-04-14 13:57:26 -0400

    read from disk

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -73,17 +73,126 @@ main:
     mov ss, ax
     mov sp, 0x7c00 ; Offset the stack to start before the code segment
 
+    ; Read something from the disk
+    mov [drive_number], dl
+    mov ax, 1
+    mov cl, 1
+    mov bx, 0x7e00
+    call disk_read
+
     ; Print a string
     mov si, msg
     call print_str
 
+    cli
     hlt
 
+; Disk read failed
+floppy_error:
+    mov si, read_failed
+    call print_str
+    jmp wait_to_reboot
+
+; Reboot the OS upon keypress
+wait_to_reboot:
+    mov ah, 0
+    int 0x16
+    jmp 0x0FFFF:0 ; Jump to the beginning of the BIOS
+
 .halt:
-    jmp .halt
+    cli
+    hlt
+
+; Convert an LBA address to a CHS address
+lba_to_chs:
+    ; Save the original values of the registers modified collaterally
+    push ax
+    push dx
+
+    ; Calculate the sector
+    xor dx, dx
+    div word [sectors_per_track]
+    inc dx
+    mov cx, dx
+
+    ; Calculate the head and cylinder
+    xor dx, dx
+    div word [heads]
+
+    ; Store the results in the registers the BIOS expects them to be in
+    mov dh, dl
+    mov ch, al
+    shl ah, 6
+    or cl, ah
+
+    pop ax
+    mov dl, al
+    pop ax
+
+    ret
+
+; Read from the disk
+disk_read:
+    ; Save the original values of the to-be modified registers
+    push ax
+    push bx
+    push cx
+    push dx
+    push di
+
+    push cx
+    call lba_to_chs
+    pop ax
+
+    mov ah, 0x02
+    mov di, 3 ; Try read operation 3 times
+
+; Retry reading from disk if failure
+.retry:
+    pusha
+    stc
+
+    int 0x13
+    jnc .done
+
+    popa
+    call disk_reset
+
+    dec di
+    test di, di
+    jnz .retry
+
+; All read attempts failed
+.failed:
+    jmp floppy_error
+
+.done:
+    popa
+
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+
+    ret
+
+; Reset the disk controller
+disk_reset:
+    pusha
+
+    mov ah, 0
+    stc
+    int 0x13
+
+    jc floppy_error
+
+    popa
+    ret
 
 ; Variables
 msg: db 'Hello, world!', ENDL, 0
+read_failed: db 'Failed to read from disk...', ENDL, 0
 
 ; BIOS stuff
 times 510-($-$$) db 0 ; Pad the program to create a 512 byte sector
